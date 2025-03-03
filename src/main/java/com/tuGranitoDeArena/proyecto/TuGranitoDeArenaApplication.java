849e7a29cc0f88f6fc94bf00527d82f2d179955c
Sebaaaa1104 <sebastiantapiacaceres@gmail.com>
2025-03-03 20:23:06 -0300

    first commit

---
+++ src/main/java/com/tuGranitoDeArena/proyecto/TuGranitoDeArenaApplication.java
@@ -0,0 +1,13 @@
+package com.tuGranitoDeArena.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuGranitoDeArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuGranitoDeArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuGranitoDeArena/proyecto/TuGranitoDeArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuGranitoDeArena.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuGranitoDeArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
