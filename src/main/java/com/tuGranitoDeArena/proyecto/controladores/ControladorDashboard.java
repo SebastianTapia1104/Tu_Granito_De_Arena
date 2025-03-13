941e2bc9ad04d841ace0cb3ffe3b1a6c1052eeb9
Luis Castellanos <luisda.betancourt@gmail.com>
2025-03-13 19:01:03 -0300

    nuevo, editar

--- src/main/java/com/tuGranitoDeArena/proyecto/controladores/ControladorDashboard.java
+++ src/main/java/com/tuGranitoDeArena/proyecto/controladores/ControladorDashboard.java
@@ -3,6 +3,7 @@ package com.tuGranitoDeArena.proyecto.controladores;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import com.tuGranitoDeArena.proyecto.servicios.ServicioProyectos;
 
 import jakarta.servlet.http.HttpSession;
 
+@Controller
 public class ControladorDashboard {
 
 	@Autowired
@@ -55,6 +57,42 @@ public class ControladorDashboard {
 		
 		return "detalle.jsp";
 	}
+	
+	@GetMapping("/nuevo") //Agregar nuevo proyecto
+	public String nuevo(HttpSession session,
+			Model model,
+			@PathVariable("id")Long id) {
+		if(session.getAttribute("usuarioEnSesion") == null){ //verificacion session
+			return "redirect:/";
+		}
+		else if(session.getAttribute("empresaEnSesion") == null){
+			return "redirect:/";
+		}
+		
+		return "nuevo.jsp";
+	}
+	
+	@GetMapping("/editar")
+	public String editar(HttpSession session,
+			Model model,
+			@PathVariable("id")Long id) {
+		if(session.getAttribute("usuarioEnSesion") == null){ //verificacion session
+			return "redirect:/";
+		}
+		else if(session.getAttribute("empresaEnSesion") == null){
+			return "redirect:/";
+		}
+		
+		//Busca proyecto con id
+		Proyecto proyecto = servProyecto.buscarProyecto(id);
+				
+		//Anade el proyecto con model y lo manda a detalle.jsp
+		model.addAttribute("proyecto", proyecto);
+		
+		return "nuevo.jsp" ;
+	}
+	
+	
 	
 	
 }
